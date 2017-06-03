d94cbc6ffdefb0ab4614225b32eaa2a7df350c77
ILIA PETROV <ilia-p@yandex.ru>
2017-06-03 17:11:05 +0300

    Upgrading in accordace with recoomendation 2017-05-28 given by Evgeny Evseev

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# Password Strength Calculator
+# Password Strength Calculator **bold**
 
-[The scrip evaluates password strength and denies password with an unappropriate string or substring]
+The scrip evaluates password strength and denies password with an unappropriate string or substring
+You just need to encript your password (you will see the proposal to input after launching) and program
+will count its rating or offer to input another one
 
-# Project Goals
+Hera are the rules you should respect: **bold**
+
+* the use of both upper-case and lower-case letters (case sensitivity)
+* inclusion of one or more numerical digits
+* inclusion of special characters, such as @, #, $
+* prohibition of words found in a password blacklist
+* prohibition of words found in the user's personal information
+* prohibition of use of company name or an abbreviation
+* prohibition of passwords that match the format of calendar dates, license plate numbers, telephone numbers, or other common numbers
+
+
+
+# Project Goals **bold**
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,26 +1,27 @@
 import re
 
-def get_password_strength(password):
+# For password rating evaluatuon
+# 1. Upper- and lower-case:
+lower_case = re.compile(r'[a-z]')
+upper_case = re.compile(r'[A-Z]')
+# 2. Digits
+digits     = re.compile(r'\d')
+# 3. Special symbols
+spec_symb  = re.compile(r'@|\^|#|\$|\*|&|-|_')
+# For password denying
+# 4. Bad passwordlist (depends on used (may be old) passwords, etc.)
+bad_password_list = ['vasya123', 'irina 1974', 'uryuk']
+# 5. Persosnel information words 
+pers_inf     = re.compile(r'ILIA|PETROV|IRINA|NIKITENKO')
+# 6. Company name (both full and acronim)
+company_name = re.compile(r'HumanFactorLabs|HFLabs|HFL')
+# 7. Standard formats
+standard_formats  = re.compile(r'\d{2}[\.-]\d{2}[\.-]\d{2,4}|\d{4}-\d{2}-\d{2}|[a-zA-Z]{1}\d{3}[a-zA-Z]{2}\d{2,3}')
+# Password strenth evaluation
+
+def get_password_rating(password):
     #     pass
     password_rating = 0
-    # For password rating evaluatuon
-    # 1. Upper- and lower-case:
-    upper_case = re.compile(r'[a-z]')
-    lower_case = re.compile(r'[A-Z]')
-    # 2. Digits
-    digits     = re.compile(r'\d')
-    # 3. Special symbols
-    spec_symb  = re.compile(r'@|\^|#|\$|\*|&|-|_')
-    # For password denying
-    # 4. Bad passwordlist (depends on used (may be old) passwords, etc.)
-    bad_password_list = ['vasya123', 'irina 1974', 'uryuk']
-    # 5. Persosnel information words 
-    pers_inf     = re.compile(r'ILIA|PETROV|IRINA|NIKITENKO')
-    # 6. Company name (both full and acronim)
-    company_name = re.compile(r'HumanFactorLabs|HFLabs|HFL')
-    # 7. Standard formats
-    standard_formats  = re.compile(r'\d{2}[\.-]\d{2}[\.-]\d{2,4}|\d{4}-\d{2}-\d{2}|[a-zA-Z]{1}\d{3}[a-zA-Z]{2}\d{2,3}')
-    # Password strenth evaluation
     if not (password in bad_password_list or
                     pers_inf.findall(password) or
                     company_name.findall(password) or
@@ -32,11 +33,15 @@ def get_password_strength(password):
             password_rating += 3
         if spec_symb.findall(password):
             password_rating += 4
+    return password_rating
+
+def result_out_put(password_rating):
+    if password_rating:
         print('\n\nYour password rating is: ', password_rating)
     else:               
         print('\n\nSuch password can not be approved, try again')
 
 if __name__ == '__main__':
-
     password = input('\nPlease, input your password:\n\n')
-    get_password_strength(password)
+    password_rating = get_password_rating(password)
+    result_out_put(password_rating)
