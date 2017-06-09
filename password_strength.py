2b42c05b8257d3bd80f011e13f7783a90b33ccf7
ILIA PETROV <ilia-p@yandex.ru>
2017-06-09 09:56:04 +0300

    Modified in accordance with comments from 07.06.2017 given by ILIA LABEDEV

--- password_strength.py
+++ password_strength.py
@@ -1,24 +1,21 @@
 import re, getpass
 
-STANDARD_FORMATS  = re.compile(r'\d{2}[\.-]\d{2}[\.-]\d{2,4}|\d{4}-\d{2}-\d{2}|[a-zA-Z]{1}\d{3}[a-zA-Z]{2}\d{2,3}')
-"""
-Standard formats 
-\d{2}[\.-]\d{2}[\.-]\d{2,4}        - Date format
-\d{4}-\d{2}-\d{2}                  - Date format
-[a-zA-Z]{1}\d{3}[a-zA-Z]{2}\d{2,3} - license plate 
-"""
+date_format_ru     = r'\d{2}[\.-]\d{2}[\.-]\d{2,4}'                    # Russian date format
+date_format_en     = r'\d{4}-\d{2}-\d{2}'                              # English date format
+license_plate      = r'[a-zA-Z]{1}\d{3}[a-zA-Z]{2}\d{2,3}'             # license format
+
 def get_password_rating(password):
     password_rating = 0
-    if not (re.compile(r'HumanFactorLabs|HFLabs|HFL').findall(password) or
-            STANDARD_FORMATS.findall(password)):
-        if (re.compile(r'[A-Z]').findall(password) and
-            re.compile(r'[a-z]').findall(password)):
+    standard_formats = re.compile('{}|{}|{}'.format(date_format_ru, date_format_en, license_plate)) 
+    if not standard_formats.findall(password):                         # Standard form serachs
+        if (re.compile(r'[A-Z]').findall(password) and                 # Upper case search
+            re.compile(r'[a-z]').findall(password)):                   # Lower case search
             letter_weight = 3
             password_rating += letter_weight
-        if re.compile(r'\d').findall(password):
+        if re.compile(r'\d').findall(password):                        # Digit search
             digit_weight = 3
             password_rating += digit_weight
-        if re.compile(r'@|\^|#|\$|\*|&|-|_|\+|=|:|;|!|<|>|~|`|\.|,|%|(|)|"|\\|/|\?').findall(password):
+        if re.compile(r'@|\^|#|\$|\*|&|-|_|\+|=|:|;|!|<|>|~|`|\.|,|%|(|)|"|\\|/|\?').findall(password): # Special symbols search
             spec_symbol_weight = 4
             password_rating += spec_symbol_weight
     return password_rating
